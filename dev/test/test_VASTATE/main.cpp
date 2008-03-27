d2bc2fe2494329b9eeb285aa480e41418dc9e404
cavour <cavour@e31416da-b748-0410-9b4e-cac2f8189b79>
2008-03-27 02:11:11 +0000

    VSM daily backup
    git-svn-id: https://vast.svn.sourceforge.net/svnroot/vast@13 e31416da-b748-0410-9b4e-cac2f8189b79

--- dev/test/test_VASTATE/main.cpp
+++ dev/test/test_VASTATE/main.cpp
@@ -4,11 +4,12 @@
 #include "shared.h"
 
 using namespace std;
+using namespace VAST;
 
-void object_info (const MValue & m)
+void object_info (MValue* m)
 {
     cout << "Object information ---" << endl;
-    cout << m.encodeToStr () << endl;
+    cout << m->encodeToStr () << endl;
     cout << "----------------------" << endl;
 }
 
@@ -17,25 +18,26 @@ int main ()
     cout << "Hello, World!" << endl;
 
     MBaseObject *mp = new MBaseObject ();
+    MBaseObject &mpr = *mp;
     // assignment
-    mp.add (0, 20);
-    mp.add (1, "test string");
+    mp->add (0, 20);
+    //mp->add (1, "test string");
     // implicit
-    mp[2] = "HP Unknown";
-    mp[5] = 77.88;
+    //mp[2] = "HP Unknown";
+    //mp[5] = 77;
+    //mp->operator [] (5).add (77);
+    mpr[5].add (77);
+    
 
-    if (mp[5] >= 10)
-        mp[2] = "HP Good!";
+//    if (mp[5] >= 10)
+//        mp[2] = "HP Good!";
 
     MBaseObject *pack = new MBaseObject ();
-    pack.add (1, "Red pack");
+    //pack->add (1, "Red pack");
+    pack->add (2, 10);
+    mpr[24] = *pack;
 
-    mp[24] = pack;
-
-    cout << "Object information ---" << endl;
-    cout << mp->encodeToStr () << endl;
-    cout << "----------------------" << endl;
-
+    object_info (mp);
     delete mp;
     return 0;
 }
