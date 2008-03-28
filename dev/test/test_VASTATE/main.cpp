733782f893c10a9b58b71d97f7a534bba8dbc0d5
cavour <cavour@e31416da-b748-0410-9b4e-cac2f8189b79>
2008-03-28 08:58:33 +0000

    new object model basic structure
    git-svn-id: https://vast.svn.sourceforge.net/svnroot/vast@16 e31416da-b748-0410-9b4e-cac2f8189b79

--- dev/test/test_VASTATE/main.cpp
+++ dev/test/test_VASTATE/main.cpp
@@ -6,38 +6,48 @@
 using namespace std;
 using namespace VAST;
 
-void object_info (MValue* m)
+MValue MValue::unknown_value;
+
+void object_info (MValue& m)
 {
     cout << "Object information ---" << endl;
-    cout << m->encodeToStr () << endl;
+    cout << m.encodeToStr () << endl;
     cout << "----------------------" << endl;
 }
 
 int main ()
 {
     cout << "Hello, World!" << endl;
-
     MBaseObject *mp = new MBaseObject ();
     MBaseObject &mpr = *mp;
     // assignment
-    mp->add (0, 20);
+    cout << "mpr.add (0, 20)" << endl;
+    mpr.add (0, 20);
+    mpr.add (5, 10);
+    cout << endl;
     //mp->add (1, "test string");
     // implicit
     //mp[2] = "HP Unknown";
-    //mp[5] = 77;
-    //mp->operator [] (5).add (77);
-    mpr[5].add (77);
-    
+    cout << "mpr[5] = 77" << endl;
+    //mpr[5] = 77;
+//    mpr[5].assign (77);
+    //mpr.getItem (5).assign (77);
+    mpr[5] = 77;
+    cout << endl;
 
 //    if (mp[5] >= 10)
 //        mp[2] = "HP Good!";
 
-    MBaseObject *pack = new MBaseObject ();
+    //MBaseObject *pack = new MBaseObject ();
     //pack->add (1, "Red pack");
-    pack->add (2, 10);
-    mpr[24] = *pack;
+    //pack->add (2, 10);
+    //mpr[24] = *pack;
 
-    object_info (mp);
+    object_info (mpr);
+    cout << "delete mp" << endl;
     delete mp;
+    cout << endl;
+
+    getchar ();
     return 0;
 }
