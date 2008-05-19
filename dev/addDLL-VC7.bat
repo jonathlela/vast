b687a9b5ddd83ba269c3f718f27ab68d323ffa9b
cavour <cavour@e31416da-b748-0410-9b4e-cac2f8189b79>
2008-05-19 06:13:04 +0000

    Directory structure adjustment
    git-svn-id: https://vast.svn.sourceforge.net/svnroot/vast@34 e31416da-b748-0410-9b4e-cac2f8189b79

--- dev/addDLL-VC7.bat
+++ dev/addDLL-VC7.bat
@@ -1,4 +1,6 @@
-copy ".\ACE_wrappers\lib.vc7\*.dll" .\bin
-copy ".\zlib\zlib1.dll" .\bin
+copy "..\ACE_wrappers\lib.vc7\*.dll" .\bin
+copy "..\zlib\zlib1.dll" .\bin
+copy "..\zlib\zlib1.dll" .\bin
+
 copy ".\sim\*.ini" .\bin
-copy ".\zlib\zlib1.dll" .\bin
+
--- dev/addDLL-VC8.bat
+++ dev/addDLL-VC8.bat
@@ -1,4 +1,5 @@
-copy ".\ACE_wrappers\lib.vc8\*.dll" .\bin
-copy ".\zlib\zlib1.dll" .\bin
+copy "..\ACE_wrappers\lib.vc8\*.dll" .\bin
+copy "..\zlib\zlib1.dll" .\bin
+copy "..\zlib\zlib1.dll" .\bin
+
 copy ".\sim\*.ini" .\bin
-copy ".\zlib\zlib1.dll" .\bin
